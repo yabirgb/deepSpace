666d3ab2b6d674d873579a815407c4e9b297b01b
Yabir <yabirgb@gmail.com>
2018-02-24 21:31:04 +0100

    Random part of the game manager

---
+++ lib/dice.rb
@@ -0,0 +1,67 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Dice
+  def initialize
+    @NHANGARSPROB = 0.25
+    @NSHIELDSPROB = 0.25
+    @NWEAPONSPROB = 0.33
+    @FIRSTSHOTPROB = 0.5
+    @generator = Random.new
+  end 
+  
+  def initWithNHangars
+    if @generator.rand <= @NHANGARSPROB
+      0
+    else
+      1
+    end
+  end
+  
+  def initWithNWeapons
+    #I use && instead of because of the precedence of operators
+    #https://stackoverflow.com/questions/1426826/difference-between-and-and-in-ruby
+
+    gen = @generator.rand
+    if gen <= @NWEAPONSPROB
+      1
+    elsif gen > @NWEAPONSPROB && gen <= 2*@NWEAPONSPROB
+            2
+    else
+      3
+    end
+  end
+  
+  def initWithShields
+    if @generator.rand <= @NSHIELDSPROB
+      0
+    else
+      1
+    end
+  end
+  
+  def whoStarts(nPlayers)
+    #Dos puntos es [a,b]
+    #Tres puntos es [a,b)
+    @generator.rand(0...nPlayers)
+  end
+  
+  def firstShot
+    if @generator.rand <= @FIRSTSHOTPROB
+      GAMECHARACTER.SPACESTATION
+    else  
+      GAMECHARACTER.ENEMYSTARSHIP
+    end
+  end
+  
+  def spaceStationMoves(speed)
+    #We assume 0 <= speed < 1 
+    
+    if @generator.rand <= speed
+      true
+    else
+      false
+    end
+  end
+end
