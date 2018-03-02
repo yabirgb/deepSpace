de1dd66f521b8a2c38565a3c8032f0bd87334c9e
Yabir <yabirgb@gmail.com>
2018-03-02 13:28:43 +0100

    Chagnes done in class

--- lib/EnemyStarShip.rb
+++ lib/EnemyStarShip.rb
@@ -1,25 +1,46 @@
-class EnemyStarShip
-  public
-  
-  def initialize(name, ammoPower, shieldPower, loot, damage)
-    @ammoPower = ammoPower
-    @name = name
-    @shieldPower = shieldPower
-    @loot = loot
-    @damage = damage
-  end
-  
-  def fire
-    @ammoPower
-  end
-  
-  def protection
-    @shieldPower
-  end
-  
-  def receiveShot(shot)
-    if protection < shot
+require_relative 'ShotResult'
+module Deepspace
+  class EnemyStarShip
+    
+    attr_reader :ammoPower
+    attr_reader :damage
+    attr_reader :loot
+    attr_reader :name
+    attr_reader :shieldPower
+    
+    
+    public
+
+    def initialize(name, ammoPower, shieldPower, loot, damage)
+      @ammoPower = ammoPower
+      @name = name
+      @shieldPower = shieldPower
+      @loot = loot
+      @damage = damage
+    end
+    
+    def newCopy(e)
+      EnemyStarShip.new(e.name, e.ammoPower, e.shieldPower, e.loot, e.damage)
+    end
+    
+    def UIversion
       
+    end
+
+    def fire
+      @ammoPower
+    end
+    
+    def protection
+      @shieldPower
+    end
+
+    def receiveShot(shot)
+      if protection < shot
+        Deepspace::ShotResult::DONOTRESIST
+      else  
+        Deepspace::ShotResult::RESIST
+      end
     end
   end
 end
