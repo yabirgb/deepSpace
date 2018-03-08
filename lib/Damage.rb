1b6db3c1ae81bb063d1a44ffdd0f86201af5b5c6
Yábir <yabirgb@gmail.com>
2018-03-08 12:14:38 +0100

    Advanced in the construction

--- lib/Damage.rb
+++ lib/Damage.rb
@@ -1,42 +1,69 @@
 require_relative 'DamageToUI'
+require_relative 'WeaponType'
 
-class Damage
-  
-  attr_getter :nShields
-  attr_getter :nWeapons
-  attr_getter :weapons
-  
-  def initialize(nshields, nweapons)
-    if
-    @nShields = nshields
-    @nWeapons = nweapons
-  end
-  
-  def newCopy(wl, s)
-    @weapons = wl
-    @nshields = s
-  end
-  
-  private
+module Deepspace
+  class Damage
+
+    attr_getter :nShields
+    attr_getter :nWeapons
+    attr_getter :weapons
+
+    def initialize(nshields, nweapons)
+      if
+        @nShields = nshields
+        @nWeapons = nweapons
+      end
+    end
+
+    def newCopy(wl, s)
+      @weapons = wl
+      @nshields = s
+    end
+
+    private
+
+    def adjust(w, s)
+      #Toma como argumento dos arrays
+      
+    end
     
-  def adjust(w, s)
-    #Toma como argumento dos arrays
+    def arrayContainsType(w, t)
+      pos = w.detect{|x| x.weapon.getType() == t}
+      if pos == nil
+        -1
+      end
+    end
+
+    public
+
+    def discardWeapon(w)
+      #Si borramos pero no está nos devuelve nil
+      if defined? @weapons then
+        @weapons.delete(w)
+      elsif @nWeapons > 0
+        @nWeapons -= 1
+      end 
+    end
     
+    def discardShieldBooster
+      @nShields -= 1 if @nShields > 0
+    end
+    
+    def hasNoEffect
+      if @nShields == 0 && @nWeapons == 0
+        true
+      else
+        false
+      end 
+    end
+
+    def UIVersion
+      DamageToUI(self)
+    end
+    
+    def to_s
+      "Weapons: #{@nWeapons}; Shields: #{@nShields}; Keep: #{@weapons}"
+    end
+
   end
-  
-  public
-  
-  def discardWeapon(w)
-    #Si borramos pero no está nos devuelve nil
-    if defined? @weapons then
-      @weapons.delete(w)
-    elsif @nWeapons > 0
-      @nWeapons -= 1
-    end 
-  end
-  
-  def UIVersion
-    DamageToUI(self)
-  end
-  
 end
