edbe80aea994e018f9a7f73fde5a67870d9b5e69
Yabir <yabirgb@gmail.com>
2018-03-30 20:25:28 +0200

    Changes to Hangar

---
+++ java/src/deepspace/Hangar.java
@@ -0,0 +1,68 @@
+/*
+ * @author Yábir García Benchakhtir
+ */
+package deepspace;
+import java.util.ArrayList;
+        
+public class Hangar implements Copyable<Hangar> {
+    
+    private int maxElements;
+    private ArrayList<Weapon> weapons;
+    private ArrayList<ShieldBooster> shieldBoosters;
+    
+    Hangar(int capacity){
+        maxElements = capacity;
+        weapons = new ArrayList<>();
+        shieldBoosters = new ArrayList<>();
+    }
+    
+    Hangar(Hangar h){
+        h = new Hangar(h.getMaxElements());
+        
+        for (Weapon w: h.getWeapons()){
+            weapons.add(new Weapon(w));
+        }
+        
+        for (ShieldBooster s: h.getShieldBoosters()){
+            shieldBoosters.add(new ShieldBooster(s));
+        }
+    }
+    
+    private boolean spaceAvailable(){
+        return getMaxElements() - weapons.size() - shieldBoosters.size() > 0;
+    }
+    
+    public boolean addWeapon(Weapon w){
+        if(spaceAvailable()){
+            weapons.add(w);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean addShieldBooster(ShieldBooster s){
+        if(spaceAvailable()){
+            shieldBoosters.add(s);
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
+    public int getMaxElements(){
+        return maxElements;
+    }
+    
+    public ArrayList<ShieldBooster> getShieldBoosters(){
+        return shieldBoosters;
+    }
+    
+    public ArrayList<Weapon> getWeapons(){
+        return weapons;
+    }
+    
+    
+    
+}
